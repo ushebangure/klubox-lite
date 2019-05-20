93ee09e8f104f49bf3e19ffd488ff7254c78fac7
Ushe Bangure <ubangure@luno.com>
2019-05-20 23:12:21 +0200

    Testing a cors fix for the frontend

--- infrastructure/middleware/cors.go
+++ infrastructure/middleware/cors.go
@@ -9,11 +9,8 @@ func Cors(next httprouter.Handle) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 		headers := w.Header()
 		headers.Add("Access-Control-Allow-Origin", "*")
-		headers.Add("Vary", "Origin")
-		headers.Add("Vary", "Access-Control-Request-Method")
-		headers.Add("Vary", "Access-Control-Request-Headers")
 		headers.Add("Access-Control-Allow-Headers", "Content-Type, Origin, Accept, token")
-		headers.Add("Access-Control-Allow-Methods", "GET, POST,OPTIONS")
+		headers.Add("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT")
 
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
